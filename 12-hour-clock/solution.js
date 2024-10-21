73fb5988eda4b3e9277512f993a59edbe0a787b7
zain-ahmed-ada <zain.ahmed@ada.ac.uk>
2024-10-21 10:56:14 +0000

    Fixed 12-hour-clock folder

--- 12-hour-clock/solution.js
+++ 12-hour-clock/solution.js
@@ -1,13 +1,13 @@
 function convertTo12HourClock(time) {
-  const hours = time.slice(0, 2);
-  const minutes = time.slice(-2);
-  if (hours > 12) {
-    return `${hours - 12}:${minutes} PM`;
-  } else {
-    return `${hours}:${minutes} AM`;
-  }
+  let [hours, minutes] = time.split(":");
+  hours = parseInt(hours);
+  const period = hours >= 12 ? "PM" : "AM";
+  hours = hours % 12 || 12;
+  return `${hours}:${minutes} ${period}`;
 }
 
 module.exports = { convertTo12HourClock };
+
+console.log(convertTo12HourClock("2:05"));
 
 // Hint: I recommend calling the function with an input of '12:05'
