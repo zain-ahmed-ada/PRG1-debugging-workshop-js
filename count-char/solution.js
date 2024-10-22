5c7e977a75c1345ec5674cd7434b62d1868058ef
zain-ahmed-ada <zain.ahmed@ada.ac.uk>
2024-10-22 19:40:06 +0000

    Refactored count-char code

--- count-char/solution.js
+++ count-char/solution.js
@@ -2,9 +2,7 @@ function countChar(string, char) {
   let count = 0;
 
   for (let i = 0; i < string.length; i++) {
-    if (string[i] === char) {
-      count++;
-    }
+    string[i] === char ? count++ : null;
   }
   return count;
 }
