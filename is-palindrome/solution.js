fdf88714280020f772f0a782d9f539de658bf2ae
zain-ahmed-ada <zain.ahmed@ada.ac.uk>
2024-10-22 21:04:33 +0000

    Fixed is-palindrome

--- is-palindrome/solution.js
+++ is-palindrome/solution.js
@@ -1,13 +1,20 @@
 function isPalindrome(str) {
   let left = 0;
   let right = str.length - 1;
+  str = str.toLowerCase();
 
   while (left < right) {
+    if (str[left] === " ") {
+      left++;
+    } else if (str[right] === " ") {
+      right--;
+    }
+
     if (str[left] !== str[right]) {
       return false;
     }
     left++;
-    right++;
+    right--;
   }
 
   return true;
@@ -15,4 +22,5 @@ function isPalindrome(str) {
 
 module.exports = { isPalindrome };
 
-console.log(isPalindrome("racecar")); // should return true
+//console.log(isPalindrome("racecar")); // should return true
+console.log(isPalindrome("racecar"));
